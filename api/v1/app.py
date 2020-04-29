e723c437983c81c97006fda9c2e491b5ea90eb2f
inoublii <inoublii@gmail.com>
2020-04-29 20:36:32 +0100

    routes

---
+++ api/v1/app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+from flask import Flask, jsonify
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+"""API"""
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+
+@app.teardown_appcontext
+def teardown_storage(exception):
+    """
+	"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    host_api = getenv("HBNB_API_HOST", "0.0.0.0")
+    port_api = getenv("HBNB_API_PORT", "5000")
+    app.run(host=host_api, port=port_api, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from api.v1.views import app_views
+rom flask import jsonify
+
+"""
+create a route
+"""
+
+@app_views.route('/status')
+def status():
+    """
+    Return  json for route
+    """
+    my_dict = {'status': "OK"}
+    return jsonify(my_dict)
