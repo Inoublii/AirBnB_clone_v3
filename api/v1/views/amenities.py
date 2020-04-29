04c8d85186ef79e6c07b0901c2a0646709928d68
rakiasomai <1069@holbertonschool.com>
2020-04-29 20:44:17 +0000

    fix

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Define Amenity Routes"""
+'''Define Amenity Routes'''
 from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models import storage
