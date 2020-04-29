f4bab96cb236eeead6a308a1a9835a217f63254f
rakiasomai <1069@holbertonschool.com>
2020-04-29 20:44:33 +0000

    fix

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -9,8 +9,8 @@ from api.v1.views import app_views
 
 @app_views.route("/states/<state_id>/cities", methods=["GET", "POST"])
 def cities(state_id):
-    """Define /cities route
-    """
+    '''Define /cities route
+    '''
     state = storage.get('State', state_id)
     if state is None:
         return abort(404)
