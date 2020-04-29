cb0d196d1432cefb8094e05137d88c9ea1868b34
inoublii <inoublii@gmail.com>
2020-04-29 21:21:59 +0100

    fx

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+"""
+Create API.
+"""
+
+
 from flask import Flask, jsonify
 from models import storage
 from flask_cors import CORS
 from api.v1.views import app_views
 from os import getenv
-"""
-API
-"""
 
 
 app = Flask(__name__)
@@ -16,22 +18,21 @@ app.register_blueprint(app_views)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
-
 @app.teardown_appcontext
 def close(exc):
     """
-    close session
+    Terminate session.
     """
     storage.close()
 
 
 @app.errorhandler(404)
 def not_found(error):
-    """Return not found """
+    """Return not found modified JSON."""
     return jsonify({'error': 'Not found'}), 404
 
 
 if __name__ == "__main__":
-    host_api = getenv("HBNB_API_HOST", "0.0.0.0")
-    port_api = getenv("HBNB_API_PORT", "5000")
-    app.run(host=host_api, port=port_api, threaded=True)
+    host = getenv("HBNB_API_HOST", default="0.0.0.0")
+    port = getenv("HBNB_API_PORT", default="5000")
+    app.run(host=host, port=port, threaded=True)
